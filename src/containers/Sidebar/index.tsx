cee62853be288231022082105b20e1fc1b4589bb
Viviane <vivianeaguiarc@outlook.com>
2024-09-09 19:01:53 -0300

    construindo a estrutura e a seção sobre mim

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/vivianezzt.png" alt="" />
+    <Titulo fontSize={20}>Viviane Aguiar</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Title'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Graduada em Análise e desenvolvimento de sitemas pela UNIASSELVI, mora em
+      Juiz de fora - MG e tem muita paixão por tecnologias e ama aprender
+      fazendo pós em Arquitetura e desenvolvimento de sistemas com ênfase em
+      padrões de projetos.
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=vivianezzt&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=vivianezzt&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
